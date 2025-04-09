a41a56e7ab5b6754f4eaa4ecaad6fbcd47334714
Larissa <larissa.dr2005@aluno.ifsc.edu.br>
2025-04-09 17:22:54 -0300

    Salvar site supershoc

---
+++ super_choque/js/menu.js
@@ -0,0 +1,13 @@
+function mostrarMenu() {
+    
+  const menulateral = document.getElementById("menu-lateral");
+  menulateral.classList.toggle("ativa");
+
+  const iconMenu = document.getElementById("img-menu");
+
+  if (iconMenu.src.endsWith("img/icon-hamburger-menu.png")){
+    iconMenu.src ="img/icon-close-menu.png";
+  } else {
+    iconMenu.src = "img/icon-hamburger-menu.png";
+  }
+}
